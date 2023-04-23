6c063d6baff163575358692321f76967c526ea95
carolnamirimu <namirimu1000@gmail.com>
2023-04-23 13:14:25 +0300

    updated the date and time

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
-function displaytemperatuer (response){
-    console.log(response.data)
-    let windElement = document.querySelector("#wind");
-    windElement.innerHTML=Math.round(response.data.wind.speed);
-let humidityElement = document.querySelector("#humidity");
-humidityElement.innerHTML=Math.round(response.data.main.humidity);
-    let descriptionElement=document.querySelector("#description");
-    descriptionElement.innerHTML= (response.data.weather[0].description);
-    let temperatuerElement = document.querySelector("#temperatuer");
-    temperatuerElement.innerHTML=Math.round(response.data.main.temp);
-    let  cityElement=document.querySelector("#city");
-    cityElement.innerHTML=(response.data.name)
-
+function formartdate(timestamp) {
+  //calculate time//
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${minutes}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours} : ${minutes}`;
 }
-let key="441d1183262ff89540a8b2407eb2ee23"
-let city = "London"
-let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
+function displaytemperatuer(response) {
+  console.log(response.data);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let temperatuerElement = document.querySelector("#temperatuer");
+  temperatuerElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formartdate(response.data.dt*1000);
+}
+let key = "441d1183262ff89540a8b2407eb2ee23";
+let city = "london";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
 
 axios.get(url).then(displaytemperatuer);

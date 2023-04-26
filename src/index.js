c0784f92f979e4825334c889992af868432ab16f
carolnamirimu <namirimu1000@gmail.com>
2023-04-26 12:20:36 +0300

    added a capital d to even.preventDfault to search engine

--- src/index.js
+++ src/index.js
@@ -1,55 +1,56 @@
 function formartdate(timestamp) {
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${minutes}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[date.getDay()];
-    return `${day} ${hours} : ${minutes}`;
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${minutes}`;
   }
-  function displaytemperatuer(response) {
-    let windElement = document.querySelector("#wind");
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = Math.round(response.data.main.humidity);
-    let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    let temperatuerElement = document.querySelector("#temperatuer");
-    temperatuerElement.innerHTML = Math.round(response.data.main.temp);
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.name;
-    let dateElement = document.querySelector("#date");
-    dateElement.innerHTML = formartdate(response.data.dt*1000);
-    let iconElement = document.querySelector("#icon");
-    iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    iconElement.setAttribute("alt", respon1se.data.weather[0].icon);
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
-  function search(city){
-    let key = "441d1183262ff89540a8b2407eb2ee23";
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours} : ${minutes}`;
+}
+function displaytemperatuer(response) {
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let temperatuerElement = document.querySelector("#temperatuer");
+  temperatuerElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formartdate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", respon1se.data.weather[0].icon);
+}
+function search(city) {
+  let key = "441d1183262ff89540a8b2407eb2ee23";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-  
-  axios.get(url).then(displaytemperatuer);
-  }
 
-  function handlesubmit(event){
-    event.preventdefault();
-    let cityinputElement = document.querySelector("#city-input");
-    search(cityinputElement.value);
-  }
-  
-  
-  let form = document.querySelector("#search-form");
-  form.addEventListener("submit", handlesubmit);
+  axios.get(url).then(displaytemperatuer);
+}
+function handlesubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handlesubmit);
